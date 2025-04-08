e7bc35ce2ab3c9de961b49604b8f0032d3cfae22
Xin Yang <yangxin0@outlook.com>
2025-04-08 12:08:44 +0800

    Load requirements from file

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
+def load_requirements():
+    with open("requirements.txt", "r") as f:
+        return f.read().splitlines()
+
 setup(
     name="repr-index",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[
-        # Add your project dependencies here
-    ],
+    install_requires=load_requirements(),
     python_requires="==3.11.*",
 ) 
